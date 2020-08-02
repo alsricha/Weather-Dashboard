092575c5dd09f58cef66ca94bf808aae31f9a5d6
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-08-02 08:46:06 -0600

    QueryURL with API Key

--- main.js
+++ main.js
@@ -18,6 +18,20 @@ function createCityList(citySearchList) {
     }
   }
 
+  function populateCityWeather(city, citySearchList) {
+    createCityList(citySearchList);
+  
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=154989ced166a2e18e091941a4f57dfc&q=" +
+      city;
+  
+    var queryURL2 =
+      "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=154989ced166a2e18e091941a4f57dfc&q=" +
+      city;
+  
+    var latitude;
+  
+    var longitude;
 
 
 
